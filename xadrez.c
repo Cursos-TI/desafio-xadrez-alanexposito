356b2e1a648ca1844436690d031c463a93a36cd3
Alan Exposito <alan25exposito@gmail.com>
2026-05-19 14:17:57 -0300

    Execução do desafio novato de xadrez. Aproveitei e adicionei opções para treinar e deixar o jogo mais completo.

--- xadrez.c
+++ xadrez.c
@@ -5,7 +5,116 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
+    
+    
+    int mov_Torre, mov_Rainha, mov_Bispo;
+    int menu1;
+    char torre_lado [10], bispo_lado [15], rainha_lado [10];
+    int torre_casas, bispo_casas, rainha_casas;
+    
+    do
+    {
+        //Menu para escolha da peça a ser mivimentada.
+        printf("\nQual peça deseja movimentar?\n");
+        printf("1 - Torre.\n");
+        printf("2 - Bispo.\n");
+        printf("3 - Rainha.\n");
+        printf("4 - Sair.\n");
+        scanf("%i", &menu1);
+        
+        //Usei um Switch para fazer o menu funcionar dependendo da escolha do usuario.
+        switch (menu1)
+        {
+            //O Usuario tem a liberdade de escolher o lado e a quantidade de casas que a peça mivimentará.
+            //Case 1 é a Torre. Foi usado FOR. 
+            case 1:
+                printf("\nQual lado deseja movimentar? Digite Esquerda ou Direita.\n");
+                scanf("%s", torre_lado);
+                printf("Quantas casas deseja andar?\n");
+                scanf("%i", &torre_casas);
+                printf("A TORRE está se movimentando %i casas para a %s\n", torre_casas, torre_lado);
+           
+                    for (int i = 0; i < torre_casas; i++)
+                    {
+                        printf("%s.\n", torre_lado);
+                    } 
+                    break;
+            //Case 2 foi usado While.
+            case 2:
+
+                printf("\nQual lado deseja movimentar? Digite Esquerda ou Direita.\n");
+                printf("Para diagonal digite esquerda,cima ou direita,baixo SEM ESPACO.\n");
+                scanf("%s", bispo_lado);
+                printf("Quantas casas deseja andar?\n");
+                scanf("%i", &bispo_casas);
+
+                //É necessario zerar (dar um reset) na variável mov_bispo, se não toda vez que rodar 
+                //vai acumulando os valores.
+                mov_Bispo = 0;
+                printf("Bispo está se movimentando %i casas para %s\n", bispo_casas, bispo_lado);
+                    while (mov_Bispo < bispo_casas)
+                    {
+                    printf("%s.\n", bispo_lado);
+                    mov_Bispo++;
+                    }
+                    
+                    break;
+            
+            case 3:        
+                
+                printf("\nQual lado deseja movimentar? Digite Esquerda ,Direita, Cima ou Baixo.\n");
+                scanf("%s", rainha_lado);
+                printf("Quantas casas deseja andar?\n");
+                scanf("%i", &rainha_casas);
+
+                //É necessario zerar (dar um reset) na variável mov_rainha, se não toda vez que rodar 
+                //vai acumulando os valores.
+                mov_Rainha = 0;
+                printf("Rainha está se movimentando %i casas para %s\n", rainha_casas, rainha_lado);
+                    do
+                    {
+                        printf("%s.\n", rainha_lado);
+                        mov_Rainha ++;
+                    } while (mov_Rainha < rainha_casas);
+                    break;
+            // Case 4 é apenas para simular a saida do jogo.
+            case 4:
+                printf("Saindo...");
+                break;
+    //default caso o usuario digite algum número deiferente das opções disponiveis.
+    default:
+        printf("Opcao invalida\n");
+        break;
+    }
+    // O While do menu principal mantém o programa rodando. Se o usuário digitar 4, 
+    // a condição torna-se falsa e o loop encerra, fechando o programa. 
+    } while (menu1 != 4);
+
+    
+    
+     /*   printf("\nMovimentando o BISPO 5 vezes para a diagonal (Cima, Direita).\n");
+        
+        while (mov_Bispo < 5)
+    {
+        
+        printf("Cima, Direita.\n");
+        mov_Bispo++;
+    } 
+    
+        printf("\nMovimentando a RAINHA 8 vezes para a esquerda.\n");
+    
+        do
+    {
+       printf("Esquerda.\n");
+        mov_Rainha ++;
+    }   while (mov_Rainha < 8);*/
+    
+
+
+    return 0;
+}
+
+// Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
     // Implementação de Movimentação do Bispo
@@ -27,6 +136,3 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
-}
